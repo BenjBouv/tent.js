9b786dcf961ccaf72aed3fffc55550f57ffc1ffc
Benjamin Bouvier <bnjbouv+github@gmail.com>
2013-04-04 18:15:31 +0200

    test-public-posts: limit and specify type post to retrieve.

--- example/test-public-posts.js
+++ example/test-public-posts.js
@@ -2,7 +2,12 @@ var tent = require('../lib/tent');
 var config = require('./config').config;
 
 var client = new tent.Client( config.stalkedEntity );
-client.postsGetPublic( {limit:1}, function(err, posts) {
+var params = {
+    limit: 1,
+    post_types: client.POSTS.essay.url
+};
+
+client.postsGetPublic( params, function(err, posts) {
     if(err) { console.error(err); } else {
         console.log('last post from stalked entity: ' + JSON.stringify(posts) );
     }
